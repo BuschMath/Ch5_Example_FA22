06a57bd61b4a28e4bb5c346ea18e87099cfad9c8
BuschMath <busch@iowacentral.edu>
2022-10-03 10:58:35 -0500

    Number Guess Nested If Else

--- Ch5_Example_FA22/main.cpp
+++ Ch5_Example_FA22/main.cpp
@@ -1,27 +1,74 @@
 #include <iostream>
+#include <stdlib.h>
+#include <time.h>
 
 using namespace std;
 
+void WelcomeMessage();
+int Ini();
+int UserGuess();
+void Evaluation(int randNum, int guess);
+
 int main()
 {
-	bool x, y;
-	int m, n;
+	WelcomeMessage();
+	int randNum = Ini();
+	int guess = UserGuess();
 
-	x = true;
-	y = false;
-	n = 1;
-
-	cout << "Enter 1 if you want the statement to be true: ";
-	cin >> m;
-	cout << endl;
-
-	if ((x && y) || (m == n))
-	{
-		cout << "The statement is true.\n";
-		cout << "Next line.\n";
-	}
-	else
-		cout << "The statement is false.\n";
+	Evaluation(randNum, guess);
 
 	return 0;
+}
+
+void WelcomeMessage()
+{
+	cout << "Welcome to the number guessing program.\n";
+	cout << "Please enter your guess integer between 1 and 10.\n";
+}
+
+int Ini()
+{
+	srand(time(NULL));
+
+	return rand() % 10 + 1;
+}
+
+int UserGuess()
+{
+	cout << "What is your guess: ";
+	int guess;
+	cin >> guess;
+	cout << endl;
+
+	return guess;
+}
+
+void Evaluation(int randNum, int guess)
+{
+	if (randNum == guess)
+	{
+		cout << "You are correct!\n";
+		return;
+	}
+	else
+	{
+		cout << "You are incorrect. :-(\n";
+		cout << "Guess again: ";
+		guess = UserGuess();
+		if (randNum == guess)
+		{
+			cout << "You are correct!\n";
+			return;
+		}
+		else
+		{
+			cout << "You are incorrect. :-(\n";
+			cout << "Last guess: ";
+			guess = UserGuess();
+			if (randNum == guess)
+				cout << "You are correct!\n";
+			else
+				cout << "You are incorrect. :-(\n";
+		}
+	}
 }
